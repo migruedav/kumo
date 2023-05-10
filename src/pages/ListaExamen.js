8f642165778dd24f44edb9337eeee2c25bb20fef
migruedav <migruedav@hotmail.com>
2023-05-09 22:09:55 -0600

    update ListaExamen.js

--- src/pages/ListaExamen.js
+++ src/pages/ListaExamen.js
@@ -36,7 +36,7 @@ function ListaExamen() {
           <div className="bg-black text-white w-1/6 rounded-l-full text-center flex items-center justify-center h-16">
             {examen.fecha.slice(8, 10)}-{examen.hora}
           </div>
-          <div className="bg-gray-600 text-white text-center w-3/6 h-16 flex items-center justify-center">
+          <div className="bg-gray-600 text-white text-center w-3/6 h-16 px-4 flex items-center justify-center">
             {examen.nombre}
           </div>
           <div className="bg-black text-white h-16 rounded-r-full w-1/6 flex justify-center items-center">
